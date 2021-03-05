192ae2c9d9622d7f865391652f805ba7a1a6b9a3
Alan Watson <placebo_al@hotmail.com>
2021-03-05 20:00:49 +1100

    Create ADuser-member-copy.ps1
    Used for copying AD member of permissions

---
+++ ADuser-member-copy.ps1
@@ -0,0 +1,11 @@
+
+param(
+    [Parameter(Mandatory)]
+    [string] $CopyToUser = (Read-Host -Prompt 'Please enter the user you want to add to: '),
+    [Parameter(Mandatory)]
+    [string] $CopyFromUser = (Read-Host -Prompt 'Please enter a username for the user you want to copy: ')
+)
+
+$CFUser = Get-ADUser $CopyFromUser -Properties MemberOf
+$CTUser = Get-ADUser $CopyToUser -Properties MemberOf
+$CFUser.MemberOf | Where-Object{$CTUser.MemberOf -notcontains $_} |  Add-ADGroupMember -Members $CTUse
