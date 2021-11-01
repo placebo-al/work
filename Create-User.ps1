711154d106ed132572974f0a83b0ec20a138e706
placebo-al <placebo_al@hotmail.com>
2021-11-01 15:59:58 +1100

    Added a credential id

--- Create-User.ps1
+++ Create-User.ps1
@@ -1,20 +1,24 @@
 #requires -module ActiveDirectory
 
-[CmdletBinding()]
-param(
-    [Parameter(Mandatory)]
-    [ValidateNotNullOrEmpty()]
-    [string]$FirstName = "alan",
+# [CmdletBinding()]
+# param(
+#     [Parameter(Mandatory)]
+#     [ValidateNotNullOrEmpty()]
+#     [string]$FirstName = "alan",
     
-    [Parameter(Mandatory)] 
-    [ValidateNotNullOrEmpty()]
-    [string]$LastName = "Watson",
+#     [Parameter(Mandatory)] 
+#     [ValidateNotNullOrEmpty()]
+#     [string]$LastName = "Watson",
     
-    [Parameter(Mandatory)]
-    [ValidateNotNullOrEmpty()] 
-    [string]$Department = "Janitor"
+#     [Parameter(Mandatory)]
+#     [ValidateNotNullOrEmpty()] 
+#     [string]$Department = "Janitor"
     
-)
+# )
+
+$FirstName = "alan"
+$LastName = "Watson"
+$Department = "Janitor"
 
 try {
     if ($LastName.length -gt 6){
@@ -23,27 +27,28 @@ try {
     else {
         $username ='{0}{1}' -f  $LastName.Substring(0,$LastName.Length),$FirstName.Substring(0,1)
     }
-    # $i=2
-    # while ((Get-ADUser -Filter "samAccountName -eq '$username'") -and ($username.Length -lt 10 )) {
-    #     Write-Warning -Message "The username [$($username)] already exists. "
-    #     Write-Warning -Message "The user account is for [$(Get-ADUser -Filter "samAccountName -eq '$username'"| Select-Object GivenName, Surname )]"
-    #     $username = '{0}{1}' -f $LastName.Substring(0,6), $FirstName.Substring(0,$i)
-    #     Start-Sleep -Seconds 2
-    #     $i++
-    # }
-    # Add-Type -AssemblyName 'System.Web'
-    $password = [System.Web.Security.Membership]::GeneratePassword((Get-Random -Minimum 20 -Maximum 32), 3)
+    $i=2
+    while ((Get-ADUser -Filter "samAccountName -eq '$username'") -and ($username.Length -lt 10 )) {
+        Write-Warning -Message "The username $($username) already exists. "
+        Write-Warning -Message "The user account is for $(Get-ADUser -Identity $username | Select-Object Name )"
+        $username = '{0}{1}' -f $LastName.Substring(0,6), $FirstName.Substring(0,$i)
+        Start-Sleep -Seconds 2
+        $i++
+    }
+    Add-Type -AssemblyName System.Web;
+    $password = [System.Web.Security.Membership]::GeneratePassword(12,3)
 
-    $newUserParams = @{
+    $newUserParams = [ordered]@{
         GivenName             = $FirstName
         Surname               = $LastName
         Name                  = $userName
+        DisplayName           = "$LastName, $FirstName"
         AccountPassword       = $password
-        ChangePasswordAtLogon = $true
-        Enabled               = $true
+        #ChangePasswordAtLogon = $true
+        #Enabled               = $true
         Department            = $Department
-        Path                  = $ou.DistinguishedName
-        Confirm               = $false
+        HomeDirectory         = "`\`\yarrahome`\users`$`\$username"
+        #Confirm               = $false
     }
 
 $newUserParams
--- Good-Morning.ps1
+++ Good-Morning.ps1
@@ -1,7 +1,8 @@
 # This is my auto start up script in PowerShell
 
-Start-Process "C:\Program Files (x86)\Microsoft Configuration Manager\AdminConsole\bin\Microsoft.ConfigurationManagement.exe" -Credential "admwatsona" -WindowStyle Minimized
-Start-Process "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Administrative Tools\Active Directory Users and Computers" -Credential "admwatsona" -WindowStyle Minimized
+$cred = Get-Credential admwatsona
+Start-Process "C:\Program Files (x86)\Microsoft Configuration Manager\AdminConsole\bin\Microsoft.ConfigurationManagement.exe" -Credential $cred -WindowStyle Minimized
+Start-Process "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Administrative Tools\Active Directory Users and Computers" -Credential $cred -WindowStyle Minimized
 
 Start-Process "C:\Program Files (x86)\Microsoft Office\root\Office16\ONENOTE.EXE" -WindowStyle Minimized
 
