e42d54b534e66e2517e2bed452065289873089ac
Alan Watson <placebo_al@hotmail.com>
2022-01-18 11:00:05 +1100

    Update profile.ps1
    Added the good-morning function

--- profile.ps1
+++ profile.ps1
@@ -2,4 +2,21 @@ Set-Location $ENV:USERPROFILE
 
 New-Alias -Name "c" -Value Clear-Host
 
+function Good-Morning {
+    $cred = Get-Credential 
+    Start-Process -Credential $cred "C:\Program Files (x86)\Microsoft Configuration Manager\AdminConsole\bin\Microsoft.ConfigurationManagement.exe" -WorkingDirectory "C:\Program Files (x86)\Microsoft Configuration Manager\AdminConsole\bin\" -WindowStyle Minimized
 
+    Start-Process -Credential $cred -FilePath "C:\Windows\System32\cmd.exe" -WorkingDirectory "C:\Windows\System32\" -ArgumentList "/c dsa.msc"
+    
+    Start-Process "C:\Program Files (x86)\Microsoft Office\root\Office16\ONENOTE.EXE" -WindowStyle Minimized
+
+    Start-Process -File "$($env:LOCALAPPDATA)\Microsoft\Teams\Update.exe" -ArgumentList '--processStart "Teams.exe"'
+
+    Start-Process "C:\Program Files (x86)\Microsoft Office\root\Office16\lync.exe"
+
+    Start-Process "C:\Program Files (x86)\Microsoft Office\root\Office16\OUTLOOK.EXE"
+
+    Start-Process "https://ictsupport.yarracity.vic.gov.au/HEAT/#1629949870611"
+
+    Start-Process "C:\Program Files (x86)\TeamViewer\TeamViewer.exe" -WindowStyle Minimized
+}
