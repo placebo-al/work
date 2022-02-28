bf67c6fa5ff97ba7583ecbc9fd268da7a68fba9b
placebo-al <placebo_al@hotmail.com>
2022-02-28 16:34:16 +1100

    Added the show printer function

--- profile.ps1
+++ profile.ps1
@@ -20,3 +20,28 @@ function Good-Morning {
 
     Start-Process "C:\Program Files (x86)\TeamViewer\TeamViewer.exe" -WindowStyle Minimized
 }
+
+function show-printer {
+
+    [cmdletbinding()]
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [string]$Printer_Id
+    )
+
+    $printer_list = Import-csv c:\users\watsona\Documents\printers.csv -delimiter '|'
+    $Selected_Printer = $printer_list | Where-Object { $_.id -like $Printer_Id }
+    if ($null -eq $Selected_Printer) {
+        Write-Output "The $Printer_Id isn't in this file"
+        break
+    }
+
+    if ($null -ne $Selected_Printer.ip) {
+        Start-Process $Selected_Printer.ip    
+    }
+    else {
+        Write-Output "This printer $Printer_Id doesn't have an ip address"
+    }
+    $Selected_Printer | Format-List
+}
